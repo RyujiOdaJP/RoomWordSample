9fb2a3dd2efc2aca1a221d92aa2e2cb1b4523c6e
ryujioda <ryuji.oda@gmail.com>
2021-03-15 08:38:51 +0900

    apply live data to DAO

--- app/src/main/java/com/example/roomwordssample/WordDao.kt
+++ app/src/main/java/com/example/roomwordssample/WordDao.kt
@@ -1,5 +1,6 @@
 package com.example.roomwordssample
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -12,7 +13,11 @@ interface WordDao {
     @Query("DELETE FROM word_table")
     fun deleteAll(): Void
 
+    //LiveData, which is a lifecycle library class for data observation,
+    // can help your app respond to data changes. If you use a return value of type LiveData
+    // in your method description, Room generates all necessary code to update
+    // the LiveData when the database is updated.
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    fun getAllWords(): List<Word>
+    fun getAllWords(): LiveData<List<Word>>
 
 }
