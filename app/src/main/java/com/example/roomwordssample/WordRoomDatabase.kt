807951bb2a050ba49f2ef5cd0fa7b9395d6a8686
ryujioda <ryuji.oda@gmail.com>
2021-03-16 07:42:14 +0900

    rm object

--- app/src/main/java/com/example/roomwordssample/WordRoomDatabase.kt
+++ app/src/main/java/com/example/roomwordssample/WordRoomDatabase.kt
@@ -8,14 +8,4 @@ import androidx.room.RoomDatabase
 @Database(entities = [Word::class], version = 1, exportSchema = false)
 abstract class WordRoomDatabase : RoomDatabase() {
     abstract fun wordDao(): WordDao
-
-    companion object {
-        private var instance: WordRoomDatabase? = null
-            get() = field ?: synchronized(this) {
-                instance ?: Room.databaseBuilder(
-                    applicationContext,
-                    WordRoomDatabase::class.java, "word_database"
-                ).build()
-            }
-    }
 }
